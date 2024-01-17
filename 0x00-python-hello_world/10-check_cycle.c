97f90fa430abf9a4296dd3cc8940d13bdd47c46c
SamKinyua <samkinyua.w@gmail.com>
2024-01-17 15:50:31 -0800

    10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * cycle_check - checks if linked list contains a cycle
+ * @list: linked list to check
+ *
+ * Return: 1 if the list has a cycle, 0 if it doesn't
+ */
+
+int cycle_check(listint_t *list)
+{
+        listint_t *one = list;
+        listint_t *two = list;
+
+        if (!list)
+                return (0);
+
+        while (one && two && two->next)
+	{
+                one = one->next;
+                two = two->next->next;
+                if (one == two)
+                        return (1);
+        }
+
+        return (0);
+}
