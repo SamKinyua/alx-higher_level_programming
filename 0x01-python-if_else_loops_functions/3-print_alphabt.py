681f307b00713add5d053a4b3ee3e24f1bbd741a
SamKinyua <samkinyua.w@gmail.com>
2024-01-17 16:21:47 -0800

    Alphabt soup

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for alphabt in range(97, 123):
     if alphabt != 113 and alphabt != 101:
-        print("{:c}".format(alphabet), end='')
+        print("{:c}".format(alphabt), end='')
