a3cd81aec21a7b6ad756384274a810d19c72b085
SamKinyua <samkinyua.w@gmail.com>
2024-02-06 16:31:44 -0800

    square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Class square for 0-square-py
+"""
+
+
+class Square:
+    """
+    class whit private attribute size
+    """
+    def __init__(self, size):
+
+        self.size__size = size
