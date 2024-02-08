8a01111a34d21d42d8c00e8f98d9b0f5dd43f0a5
SamKinyua <samkinyua.w@gmail.com>
2024-02-07 18:27:32 -0800

    corrections

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -58,12 +58,15 @@ class Square:
             print("")
 
     def __str__(self):
-        """Define the print() representation of a Square."""
-        if self.__size != 0:
-            [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
-            if i != self.__size - 1:
-                print("")
-        return ("")
+        """Return a string representation of the square."""
+    if self.__size == 0:
+        return ""
+
+    square_str = ""
+    for _ in range(self.__position[1]):
+        square_str += "\n"
+
+    for _ in range(self.__size):
+        square_str += " " * self.__position[0] + "#" * self.__size + "\n"
+
+    return square_str
