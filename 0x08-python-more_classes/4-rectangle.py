317223ec196fc0719548b68aee0b1abcb088fc06
SamKinyua <samkinyua.w@gmail.com>
2024-02-12 15:15:49 -0800

    eval magic

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -59,7 +59,7 @@ class Rectangle:
             return ("")
 
         rect = []
-        for i in range(self.__heigth):
+        for i in range(self.__height):
             [rect.append('#') for j in range(self.__width)]
             if i != self.__height - 1:
                 rect.append("\n")
